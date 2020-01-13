55c1fcb79533c937b9604592e005077271c9aebe
namtd <namtrandinh@vccorp.vn>
2020-01-13 10:57:05 +0700

    add image

--- scripts/egg-class.js
+++ scripts/egg-class.js
@@ -1,123 +1,114 @@
-// Download DOM before running jQuery script
-$(document).ready(onLoad);
-
-var scope = {};
-// test creating new test
-
-// This is a test function to work with the egg
-// function onLoad(){
-
-//     var firstChickenDiv = $('div.chicken').first();
-
-//     scope.testEgg = new Egg(firstChickenDiv);
-//     console.log(scope.testEgg.startPosLeft);
-//     window.setTimeout(function(){scope.testEgg.startFall();},1000);
-
-
-// }
-
-
-
 // Create Egg Class in a separate js files as a prototype
 // for mulitple eggs
 
 // Create Class Egg with parameter $chickenDiv
 // because I need to know where to create the egg div.
-function Egg($chickenDiv){
-  // this. is used to point to individual object
-  // of future egg objects
-  // position: https://api.jquery.com/position/
-  this.chickenDivPosition = $chickenDiv.offset();
-  this.startPosLeft = this.chickenDivPosition.left + $chickenDiv.width()/2 - 20;
-  this.startPosTop = this.chickenDivPosition.top + $chickenDiv.height() - 30;
+function Egg($chickenDiv)
+{
+    // this. is used to point to individual object
+    // of future egg objects
+    // position: https://api.jquery.com/position/
+    //Set vị trí cho trứng rơi
+    this.chickenDivPosition = $chickenDiv.offset();
+    this.startPosLeft       = this.chickenDivPosition.left + $chickenDiv.width() / 2 - 20;
+    this.startPosTop        = this.chickenDivPosition.top + $chickenDiv.height() - 30;
 
 
+    // create div in memory to hold egg image
+    // set width, height and background via class egg in egg_style.css
+    // set left and top position via input parameters
+    this.$eggImageDiv = $("<div></div>").attr("class", "egg").css("left", this.startPosLeft).css("top", this.startPosTop);
 
-  // create div in memory to hold egg image
-  // set width, height and background via class egg in egg_style.css
-  // set left and top position via input parameters
-  this.$eggImageDiv = $('<div></div>').attr('class', 'egg')
-                                      .css('left', this.startPosLeft)
-                                      .css('top', this.startPosTop);
+    $chickenDiv.append(this.$eggImageDiv);
+    // calculate distance of egg fall by subtracting it's position from the window
+    var basket            = new Basket();
+    this.distanceToBasket = basket.basketTop;
 
-  $chickenDiv.append(this.$eggImageDiv);
-  // calculate distance of egg fall by subtracting it's position from the window
-  var basket = new Basket();
-  this.distanceToBasket = basket.basketTop;
+    //create function startFall to animate egg drop and find out the height
+    // of the window and myltipy by to 10 to control speed.
+    // this.breakEgg is a callback function that will called after
+    // egg animation is complete
+    var milliSeconds = window.innerHeight * (scope.speed - scope.level);
 
-  //create function startFall to animate egg drop and find out the height
-  // of the window and myltipy by to 10 to control speed.
-  // this.breakEgg is a callback function that will called after
-  // egg animation is complete
-  var milliSeconds = window.innerHeight * 15;
+    //capture 'this' into separate variable to use it for
+    // further calcution. 'this' will change its meaning inside
+    // local functions and loops therefore we need to capture it in
+    // a separate variabe. Credit for 'var basketObject = 'this' goes to:
+    // http://stackoverflow.com/questions/520019/controlling-the-value-of-this-in-a-jquery-event
 
-  //capture 'this' into separate variable to use it for
-  // further calcution. 'this' will change its meaning inside
-  // local functions and loops therefore we need to capture it in
-  // a separate variabe. Credit for 'var basketObject = 'this' goes to:
-  // http://stackoverflow.com/questions/520019/controlling-the-value-of-this-in-a-jquery-event
+    var eggObject = this;
 
-  var eggObject = this;
-
-  this.startFall = function(){
-    this.$eggImageDiv.animate({top: this.distanceToBasket}
-                            , milliSeconds, 'linear', this.catchEgg);
-  };
+    this.startFall = function ()
+    {
+        this.$eggImageDiv.animate({top: this.distanceToBasket}, milliSeconds, "linear", this.catchEgg);
+    };
 
 
-// this.breakEgg is a callback function triggered by animate function
-// after animation is complete
-// remove() is opposite of append to remove from DOM
-  this.catchEgg = function(){
-    //$(this).fadeOut('slow', function(){$(this).remove();});
-    var $eggDiv = $(this);
-    // $eggDiv.css('border', '2px solid purple');
-    var $eggPosition = $eggDiv.offset();
+    // this.breakEgg is a callback function triggered by animate function
+    // after animation is complete
+    // remove() is opposite of append to remove from DOM
+    this.catchEgg = function ()
+    {
+        if(scope.life>0){
+            // $(this).fadeOut('slow', function(){$(this).remove();});
+            var $eggDiv      = $(this);
+            // $eggDiv.css('border', '20px solid purple');
+            var $eggPosition = $eggDiv.offset();
 
-    var basket = new Basket();
-    var basketRight = basket.basketRight;
-    var basketLeft = basket.basketLeft;
-    var eggLeft = $eggPosition.left;
-    var eggRight = $eggPosition.left + $eggDiv.width();
+            var basket      = new Basket();
+            var basketRight = basket.basketRight;
+            var basketLeft  = basket.basketLeft;
+            var eggLeft     = $eggPosition.left;
+            var eggRight    = $eggPosition.left + $eggDiv.width();
+            console.log(basketRight,eggRight,"bắt");
+            var isCatched = basketRight > eggRight && basketLeft < eggLeft;
 
-    var isCatched = basketRight > eggRight
-                    && basketLeft < eggLeft;
-    if(isCatched){
-      $eggDiv.fadeOut('slow', function(){$eggDiv.remove();});
-      // credit parseInt Vladimir Cherny
-      $('div.score').text(parseInt($('div.score').text()) + 1);
-
-    }else{
-      eggObject.missedEgg();
-    }
-
-
-
-
-
-
-
-  };
-
-
-  this.missedEgg = function(){
-    var $egg = eggObject.$eggImageDiv;
-    $egg.animate({top: window.innerHeight - $egg.height()}
-                            , 1000, 'linear', eggObject.breakEgg);
-
-
-
-  }
-
-  this.breakEgg = function(){
-    var score = $('div.score').text();
-    var player = $.urlParam('player');
-    $(this).remove();
-    // $(document.location).attr('href','egg_game_pg_1.html?score=' + score + '&player=' + player);
+            if (isCatched)
+            {
+                $eggDiv.fadeOut("slow", function () {$eggDiv.remove();});
+                scope.score += scope.point;
+                let current_level = scope.score % scope.score_per_lever;
+                if(current_level == 0){
+                    if(scope.level < scope.level_max){
+                        scope.level++;
+                    }else{
+                        scope.level = scope.level_max;
+                    }
+                }
+                $("div.score").text(scope.score);
+            } else
+            {
+                eggObject.missedEgg();
+            }
+            chickensStartHatch();
+        }
+    };
 
 
-  };
+    this.missedEgg = function ()
+    {
+        var $egg = eggObject.$eggImageDiv;
+        $egg.animate({top: window.innerHeight - $egg.height()}, 1000, "linear", eggObject.breakEgg);
+        scope.life = (--scope.life < 0) ? 0 : scope.life;
+    };
 
+    this.breakEgg = function ()
+    {
+        var player   = $.urlParam("player");
+        //Lấy ra div life đầu tiên để remove
+        let $divLife = $(scope.$lifeContainer).find(">:first-child");
+        $($divLife).remove();
+        //remove trứng đã rơi
+        $(this).remove();
+        if (scope.life <= 0)
+        {
+            $("div.egg").remove();
+            alert("Chúc mừng, bạn đã được "+scope.score+" điểm!");
+        }
+        // $(document.location).attr('href','egg_game_pg_1.html?score=' + score + '&player=' + player);
+
+
+    };
 
 
 };
@@ -125,16 +116,18 @@ function Egg($chickenDiv){
 
 // plugin function from http://snipplr.com/view/26662/get-url-parameters-with-jquery--improved/
 
-$.urlParam = function(name) {
+$.urlParam = function (name)
+{
 
-     url = window.location.href;
+    url = window.location.href;
 
-    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
-    if (!results) {
+    var results = new RegExp("[\\?&]" + name + "=([^&#]*)").exec(url);
+    if (!results)
+    {
         return undefined;
     }
     return unescape(results[1]) || undefined;
-}
+};
 
 
 
--- scripts/egg_game_pg_2.js
+++ scripts/egg_game_pg_2.js
@@ -1,95 +1,113 @@
-// jQuery for Catch the Eggs Game
-
-
 // Download DOM before running jQuery script
 $(document).ready(onLoad);
 
 // Create global object so jQuery doesn't have to
 // repeat the search for elements in the DOM
 var scope = {};
-
+//Event click button play
+$(document).on("click","#play",function (e) {
+    let username = $("#username").val();
+    let tel      = $("#tel").val();
+    let tel_regex = new RegExp('^\\(?([0-9]{3})\\)?([ .-]?)([0-9]{3})\\2([0-9]{4,5})$');
+    if(username == ""){
+        alert("Bạn chưa nhập họ tên");
+    }else if(!tel_regex.test(tel)){
+        alert("Số điện thoại không đúng định dạng");
+    }else{
+        // start hatching eggs for all chicken divs
+        window.setTimeout(chickensStartHatch, 100);
+    }
+    e.preventDefault();
+});
 // begin jQuery execution
-function onLoad(){
-
-    scope.$wolf = $('div.wolf');
-    scope.$allChickens = $('div.chicken');
-    scope.pointer_width = 0;
-    scope.wolf_width = $(".wolf").width();
-    scope.basket_width = $(".basket").width();
+function onLoad()
+{
+    let i;
+    scope.$wolf            = $("div.wolf");
+    scope.$allChickens     = $("div.chicken");
+    scope.$lifeContainer   = $("div.life-container");
+    scope.$life            = `<div class="life"></div>`;
+    scope.pointer_width    = 0;
+    scope.wolf_width       = $(".wolf").width();
+    scope.basket_width     = $(".basket").width();
+    //Số điểm tăng thêm khi hứng được
+    scope.point            = 10;
+    //Số mạng chơi
+    scope.life             = 3;
+    //Số điểm
+    scope.score            = 0;
+    //Số điểm tăng cấp mỗi level
+    scope.score_per_lever  = 100;
+    //Level lúc đầu
+    scope.level            = 1;
+    //Level max
+    scope.level_max        = 5;
+    //Tốc độ(milliseconds)
+    scope.speed            = 10;
     //Vị trí con trỏ trên cái giỏ
     scope.pointer_position = scope.basket_width / 2;
     //Position của phần tử cha
-    scope.parentRect = $(".container")[0].getBoundingClientRect();
-
-// add keydown event listener to move basket
-  $(document).keydown(moveWolf);
-
-// start hatching eggs for all chicken divs
-    window.setTimeout(chickensStartHatch, 100);
+    scope.parentRect       = $("div.game")[0].getBoundingClientRect();
+    //Set life
+    for(i =1;i<=scope.life;i++){
+        let life = $(scope.$life).addClass("life-"+i);
+        $(scope.$lifeContainer).append($(life));
+    }
 };
 
 //add mouse over event to move basket
-$(document).on('mousemove', function (e) {
+$(document).on("mousemove", function (e)
+{
     //Nếu con trỏ vượt qua mép trái or phải thì ko di chuyển giỏ
-    if(e.pageX-scope.pointer_position> scope.parentRect.left && e.pageX +scope.pointer_position < scope.parentRect.right ){
-        scope.$wolf.css('left', e.pageX-scope.pointer_position)
+    if (e.pageX - scope.pointer_position > scope.parentRect.left && e.pageX + scope.pointer_position <
+        scope.parentRect.right)
+    {
+        scope.$wolf.css("left", e.pageX - scope.pointer_position);
     }
 });
+
 //add touchmove event to move basket on mobile
-$(document).on('touchmove', function (e) {
+$(document).on("touchmove", function (e)
+{
     let cursor = e.originalEvent.touches[0].pageX;
     //Nếu con trỏ vượt qua mép trái or phải thì ko di chuyển giỏ
-    if(cursor-scope.pointer_position > scope.parentRect.left && cursor +scope.pointer_position < scope.parentRect.right ){
-        scope.$wolf.css('left', cursor-scope.pointer_position)
+    if (cursor - scope.pointer_position > scope.parentRect.left && cursor + scope.pointer_position <
+        scope.parentRect.right)
+    {
+        scope.$wolf.css("left", cursor - scope.pointer_position);
     }
 });
 
-function HorizontallyBound($parentDiv, $childDiv) {
-    var parentRect = $parentDiv[0].getBoundingClientRect();
-    var childRect = $childDiv[0].getBoundingClientRect();
-    return childRect.left+50 < parentRect.left || childRect.right>= parentRect.right;
-}
-
-
-
-function getOffset(el) {
+function getOffset(el)
+{
     const rect = el.getBoundingClientRect();
     return {
         left: rect.left + window.scrollX,
-        top: rect.top + window.scrollY
+        top : rect.top + window.scrollY
     };
 }
-
- // create event handler for keydown event
-function moveWolf(event){
-  // check for right and left arrow keys
-  // if right key then move basket to the right
-  if (event.key == 'ArrowRight'){
-    scope.$wolf.css('left', '+=50').removeClass('wolf-left');
-// if left key then move basket to the left
-  } else if (event.key == 'ArrowLeft'){
-    scope.$wolf.css('left', '-=50').addClass('wolf-left');
-  }
-};
 
 
 // this.startPos = chickens[Math.floor(Math.random()*4)].position()
 
 // select all 4 chicken divs
-function chickensStartHatch(){
-  var $allChickenDivs = $('div.chicken')
-// use .each to execute function for each element in the array of chicken divs
-                        .each(function(index) {
-// 'this' in context of enclosed function represents each html element
-                                  var $eachChickenDiv = $(this);
-// create object chicken object from Chicken clas
-                                  var $eachChickenObject = new Chicken($eachChickenDiv);
-// envoke method hatchEggs in each instance of Chicken Class
-                                  window.setTimeout(function(){
-                                       $eachChickenObject.hatchEggs();
-                                     }, (index + 1) * 5000
-                                  );
-                              });
+function chickensStartHatch()
+{
+    //use .each to execute function for each element in the array of chicken divs
+    scope.$allChickens.each(function (index)
+          {
+              // Lấy ngẫu nhiên 1 trong các div chicken để ấp trứng
+              let random_index = Math.floor(Math.random()*scope.$allChickens.length);
+              let $eachChickenDiv    = $(scope.$allChickens.splice(random_index,1));
+              // var $eachChickenDiv    = $($(scope.$allChickens)[Math.floor(Math.random()*scope.$allChickens.length)]);
+              // create object chicken object from Chicken class
+              let $eachChickenObject = new Chicken($eachChickenDiv);
+              // Sau (random_index+1)*2 giây hàm ấp trứng sẽ thực hiện
+              window.setTimeout(function (){
+                                    $eachChickenObject.hatchEggs();
+                                }, (index + 1) * 2000
+              );
+          });
 
 }
 
