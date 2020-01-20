513b5d5641c9128c8cb2f8707a871871a8c7145b
namtd <namtrandinh@vccorp.vn>
2020-01-20 14:19:56 +0700

    fix

--- scripts/game.js
+++ scripts/game.js
@@ -45,7 +45,7 @@ function onLoad()
         let life = $(scope.$life).addClass("life-" + i);
         $(scope.$lifeContainer).append($(life));
     }
-    if(!CSS.supports('justify-content', 'space-around')||!CSS.supports('justify-content', 'space-evenly')){
+    if(!CSS.supports('justify-content')||CSS.supports("display: flex")){
         alert("Trình duyệt của bạn ko hỗ trợ, xin hay đổi trình duyệt hoặc cập nhập");
     }
     console.log(CSS.supports('display', 'flex'));
