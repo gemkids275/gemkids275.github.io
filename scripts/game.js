4afbb080f785e710e578ea519e9f4de52844c497
namtd <namtrandinh@vccorp.vn>
2020-01-20 14:45:09 +0700

    fix css

--- scripts/game.js
+++ scripts/game.js
@@ -45,6 +45,7 @@ function onLoad()
         let life = $(scope.$life).addClass("life-" + i);
         $(scope.$lifeContainer).append($(life));
     }
+    alert(CSS.supports("justify-content:space-around"));
     if(!CSS.supports("justify-content:space-around")){
         $.confirm({
                       title: 'Chú ý !',
