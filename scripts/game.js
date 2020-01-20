398f0dd184c41b42b673e5175b045ebf586a1936
namtd <namtrandinh@vccorp.vn>
2020-01-20 14:30:02 +0700

    alert

--- scripts/game.js
+++ scripts/game.js
@@ -45,11 +45,10 @@ function onLoad()
         let life = $(scope.$life).addClass("life-" + i);
         $(scope.$lifeContainer).append($(life));
     }
-    alert(CSS.supports('justify-content:space-around'));
-    if(!CSS.supports('justify-content')||!CSS.supports("display: flex")){
-        alert("Trình duyệt của bạn ko hỗ trợ, xin hay đổi trình duyệt hoặc cập nhập");
+    if(!CSS.supports(`(justify-content:space-around) or (justify-content:space-evenly) or (display:flex)`)){
+        warning("Trình duyệt của bạn không hỗ trợ hiệu ứng, đổi máy hoặc trình duyệt!");
+        redirectToHome();
     }
-    console.log(CSS.supports('justify-content:space-around'));
 
     if(scope.user.phone != null ) {
         if(!scope.user.phone.match(/^[0-9]{10}$/g)) {
